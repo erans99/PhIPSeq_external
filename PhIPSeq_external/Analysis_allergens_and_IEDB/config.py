e64ee0730a1ff51d256ca0795f851c9b46bbc94a
kalkairis <kalka.iris@gmail.com>
2021-12-07 15:57:38 +0200

    adding eggs subplot

--- PhIPSeq_external/Analysis_allergens_and_IEDB/config.py
+++ PhIPSeq_external/Analysis_allergens_and_IEDB/config.py
@@ -1,6 +1,6 @@
 import os
 
-base_path = "/Users/iriskalka/Dropbox (Weizmann Institute)/Phi-Seq/temporary_files/allergens" # directory with exported files: library_contents.csv, cohort.csv & fold_data.csv
+base_path = "XXXX"  # directory with exported files: library_contents.csv, cohort.csv & fold_data.csv
 out_path = os.path.join(base_path, "analysis_res")
 
 MIN_OLIS = 200
