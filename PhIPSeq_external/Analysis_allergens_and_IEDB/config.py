4b207d03fdd0ed6036782cc271a2620a6fca5d5a
kalkairis <kalka.iris@gmail.com>
2021-12-08 13:17:25 +0200

    add config_local option

--- PhIPSeq_external/Analysis_allergens_and_IEDB/config.py
+++ PhIPSeq_external/Analysis_allergens_and_IEDB/config.py
@@ -1,4 +1,9 @@
 import os
 
-base_path = "XXXX" # directory with exported files: library_contents.csv, cohort.csv & fold_data.csv
-out_path = os.path.join(base_path, "analysis_res")
+if os.path.exists(__file__.replace('.py', '_local.py')):
+    # noinspection PyUnresolvedReferences
+    from config_local.py import *
+
+base_path = globals().get('base_path',
+                          "XXXX")  # directory with exported files: library_contents.csv, cohort.csv & fold_data.csv
+out_path = globals().get('out_path', os.path.join(base_path, "analysis_res"))
